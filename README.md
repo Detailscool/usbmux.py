a0237e1af2efd486371f97fd6df0158d5a8745d1
Detailscool <496389006@qq.com>
2022-10-01 20:14:15 +0800

    Support Python3.9

---
+++ README.md
@@ -0,0 +1,5 @@
+# usbmux-python-client
+python client of https://cgit.sukimashita.com/usbmuxd.git
+
+# Introduction
+Modified version of usbmux/tcprelay python wrapper and script to add support for multiple attached devices (Python >= 3.9)
--- tcprelay.py
+++ tcprelay.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 #
-#    tcprelay.py - TCP connection relay for usbmuxd
+#	tcprelay.py - TCP connection relay for usbmuxd
 #
-# Copyright (C) 2009    Hector Martin "marcan" <hector@marcansoft.com>
+# Copyright (C) 2009	Hector Martin "marcan" <hector@marcansoft.com>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -19,11 +19,11 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 import usbmux
-import SocketServer
+import socketserver
 import select
 from optparse import OptionParser
 import sys
-import threading
+
 
 class SocketRelay(object):
     def __init__(self, a, b, maxbuf=65535):
@@ -32,6 +32,7 @@ class SocketRelay(object):
         self.atob = ""
         self.btoa = ""
         self.maxbuf = maxbuf
+
     def handle(self):
         while True:
             rlist = []
@@ -49,99 +50,72 @@ class SocketRelay(object):
             if xlo:
                 return
             if self.a in wlo:
-                n = self.a.send(self.btoa)
+                n = self.a.send(self.btoa.encode('ISO-8859-1'))
                 self.btoa = self.btoa[n:]
             if self.b in wlo:
-                n = self.b.send(self.atob)
+                n = self.b.send(self.atob.encode('ISO-8859-1'))
                 self.atob = self.atob[n:]
             if self.a in rlo:
                 s = self.a.recv(self.maxbuf - len(self.atob))
                 if not s:
                     return
+                s = s.decode('ISO-8859-1')
                 self.atob += s
             if self.b in rlo:
                 s = self.b.recv(self.maxbuf - len(self.btoa))
                 if not s:
                     return
+                s = s.decode('ISO-8859-1')
                 self.btoa += s
-            #print "Relay iter: %8d atob, %8d btoa, lists: %r %r %r"%(len(self.atob), len(self.btoa), rlo, wlo, xlo)
+        # print "Relay iter: %8d atob, %8d btoa, lists: %r %r %r"%(len(self.atob), len(self.btoa), rlo, wlo, xlo)
 
-class TCPRelay(SocketServer.BaseRequestHandler):
+
+class TCPRelay(socketserver.BaseRequestHandler):
     def handle(self):
-        print "Incoming connection to %d"%self.server.server_address[1]
+        print("Incoming connection to %d" % self.server.server_address[1])
         mux = usbmux.USBMux(options.sockpath)
-        print "Discovering devices..."
-        devcount = len(mux.devices)
-        while True:
-            mux.process(0.1)
-            cnt = len(mux.devices)
-            if cnt > devcount:
-                devcount = cnt
-            else:
-                break
-
+        print("Waiting for devices...")
         if not mux.devices:
-            print "No devices found"
+            mux.process(1.0)
+        if not mux.devices:
+            print("No device found")
             self.request.close()
             return
-
-        # select a device
-        dev=None
-        if self.server.device:
-            for d in mux.devices:
-                if d.serial == self.server.device:
-                    dev = d
-                    break
-
-            if not dev:
-                print "Unable to find device: %s" % self.server.device
-                self.request.close()
-                return
-        else:
-            dev = mux.devices[0]
-
-        print "Connecting to device %s"%str(dev)
+        dev = mux.devices[0]
+        print("Connecting to device %s" % str(dev))
         dsock = mux.connect(dev, self.server.rport)
         lsock = self.request
-        print "Connection established, relaying data"
+        print("Connection established, relaying data")
         try:
             fwd = SocketRelay(dsock, lsock, self.server.bufsize * 1024)
             fwd.handle()
         finally:
             dsock.close()
             lsock.close()
-        print "Connection closed"
+        print("Connection closed")
 
-class TCPServer(SocketServer.TCPServer):
+
+class TCPServer(socketserver.TCPServer):
     allow_reuse_address = True
-    device = None
 
-class ThreadedTCPServer(SocketServer.ThreadingMixIn, TCPServer):
+
+class ThreadedTCPServer(socketserver.ThreadingMixIn, TCPServer):
     pass
 
-def do_list():
-    mux = usbmux.USBMux()
-    print "Listing devices..."
-    devcount = len(mux.devices)
-    while True:
-        mux.process(0.1)
-        cnt = len(mux.devices)
-        if cnt > devcount:
-            devcount = cnt
-        else:
-            break
-    for dev in mux.devices:
-        print dev
 
+if sys.version_info < (3, 9):
+    print('Only For Version Over Python3.9')
+    sys.exit(1)
 
 HOST = "localhost"
 
-parser = OptionParser(usage="usage: %prog [OPTIONS] RemotePort[:LocalPort][-UDID] [RemotePort[:LocalPort][-UDID]...")
-parser.add_option("-t", "--threaded", dest='threaded', action='store_true', default=False, help="use threading to handle multiple connections at once")
-parser.add_option("-l", "--list", dest='list', action='store_true', default=False, help="list attached devices")
-parser.add_option("-b", "--bufsize", dest='bufsize', action='store', metavar='KILOBYTES', type='int', default=128, help="specify buffer size for socket forwarding")
-parser.add_option("-s", "--socket", dest='sockpath', action='store', metavar='PATH', type='str', default=None, help="specify the path of the usbmuxd socket")
-parser.add_option("-c", "--config", dest='config', action='store', metavar='CONFIG', type='str', default=None, help="specify config file for relays")
+parser = OptionParser(usage="usage: %prog [OPTIONS] RemotePort[:LocalPort] [RemotePort[:LocalPort]]...")
+parser.add_option("-t", "--threaded", dest='threaded', action='store_true', default=False,
+                  help="use threading to handle multiple connections at once")
+parser.add_option("-b", "--bufsize", dest='bufsize', action='store', metavar='KILOBYTES', type='int', default=128,
+                  help="specify buffer size for socket forwarding")
+parser.add_option("-s", "--socket", dest='sockpath', action='store', metavar='PATH', type='str', default=None,
+                  help="specify the path of the usbmuxd socket")
 
 options, args = parser.parse_args()
 
@@ -149,47 +123,31 @@ serverclass = TCPServer
 if options.threaded:
     serverclass = ThreadedTCPServer
 
-if options.list:
-    do_list()
-    sys.exit(0)
-
-if options.config:
-    f = open(options.config)
-    args += f.readlines()
-    f.close
-
 if len(args) == 0:
     parser.print_help()
     sys.exit(1)
 
 ports = []
 
-
 for arg in args:
-    arg = arg.rstrip("\n")
     try:
-        dev = None
-        if '-' in arg:
-            arg, dev = arg.split('-')
-
         if ':' in arg:
             rport, lport = arg.split(":")
             rport = int(rport)
             lport = int(lport)
-            ports.append((rport, lport, dev))
+            ports.append((rport, lport))
         else:
             ports.append((int(arg), int(arg)))
     except:
         parser.print_help()
         sys.exit(1)
 
-servers=[]
+servers = []
 
-for rport, lport, dev in ports:
-    print "Forwarding local port %d to remote port %d on device %s"%(lport, rport, dev)
+for rport, lport in ports:
+    print("Forwarding local port %d to remote port %d" % (lport, rport))
     server = serverclass((HOST, lport), TCPRelay)
     server.rport = rport
-    server.device = dev
     server.bufsize = options.bufsize
     servers.append(server)
 
